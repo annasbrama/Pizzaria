83bce29d3347d6cb04a5891c42cf507f3044709d
Anna <acesbrama@gmail.com>
2024-01-30 22:22:29 -0300

    Adição da pasta script e novas animações ativadas

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+/* Códigos da Animação */
+
+
+
+$('section.pizza').waypoint(function (direcao) /*direcao = representa um espaço reservado na memoria que irá guardar como o usuário chegou naquele elemento html, as duas possibilidades serão up ou down*/ {
+    if (direcao == 'down') {
+        $('section.pizza p').addClass('animate__animated animate__fadeInUp');
+    }
+    if (direcao == 'up') {
+        $('section.pizza p').removeClass('animate__animated animate__fadeInUp');
+    }
+
+}, { offset: '450px;' })
